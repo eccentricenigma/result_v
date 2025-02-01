0e499c8c4789f6fafddf21219d54835ad152c9fe
enigma <eccentricenigma0@gmail.com>
2025-02-01 23:09:55 +0100

    UI buildup 5

--- add_courses.py
+++ add_courses.py
@@ -4,19 +4,26 @@ class CourseDetails(customtkinter.CTkToplevel):
     def __init__(self, master):
         super().__init__(master)
         self.title("Add Course")
-        self.state("zoomed")
+        # self.state("zoomed")
+        self.resizable(0, 0)
+        self.geometry("800x800")
         self.cou_det = ["Course", "Subject title"]
         
+        label = customtkinter.CTkLabel(self, 
+                                       text = "PLEASE FILL IN THE GIVEN SPACES ACCURATELY WITH THE COURSES",
+                                       font = ("Times", 20))
+        label.grid(row = 0, column = 0, columnspan = 2, padx = 50, pady = 15, sticky = "nsew")
         
         label = customtkinter.CTkLabel(self, text = "Course")
-        label.grid(row = 1, column = 0, padx = 20, pady = 20, sticky="nsew")
+        label.grid(row = 1, column = 0, padx = 25, pady = 15, sticky="nsew")
         
         label = customtkinter.CTkLabel(self, text = "Subject Title")
-        label.grid(row = 1, column = 1, padx = 20, pady = 20, sticky="nsew")
+        label.grid(row = 1, column = 1, padx = 25, pady = 15, sticky="nsew")
             
-        #Loop to add entry and combo boxe widgets for each of the items in the self.cou_det list    
         for x in range(10):
             for y in range(2):
-                #This adds the entry widgets
-                new_entry = customtkinter.CTkEntry(self, width = 130)
-                new_entry.grid(row = x + 2, column = y, padx = 10, pady = 20, sticky="nsew")
+                new_entry = customtkinter.CTkEntry(self, width = 350)
+                new_entry.grid(row = x + 2, column = y, padx = 25, pady = 15, sticky="nsew")
+        
+        add_rec = customtkinter.CTkButton(self, text = "Add Courses")
+        add_rec.grid(row = 12, column = 0, columnspan = 2, padx = 25, pady = 15, sticky = "nsew")
--- add_student.py
+++ add_student.py
@@ -5,15 +5,21 @@ class StudentDetails(customtkinter.CTkToplevel):
         super().__init__()
         self.title("Add Student")
         self.state("zoomed")
+        # self.columnconfigure(0, weight = 1)
         customtkinter.set_appearance_mode("light")
         customtkinter.set_default_color_theme("dark-blue")
+        
+        label = customtkinter.CTkLabel(self, 
+                                       text = "PLEASE FILL IN THE GIVEN SPACES ACCURATELY WITH THE STUDENT'S DATA",
+                                       font = ("Times", 20))
+        label.grid(row = 0, column = 1, columnspan = 2, padx = 10, pady = 20, sticky = "nsew")
         
         self.stu_det = ["First name", "Middle name (optional)", " Surname", "Examination number"]
         
         for x in range(10):
             for y in range(len(self.stu_det)):
-                details = customtkinter.CTkEntry(self, placeholder_text = self.stu_det[y], width = 180)
-                details.grid(row = x + 1, column = y + 1, padx = 10, pady = 20)
+                details = customtkinter.CTkEntry(self, placeholder_text = self.stu_det[y], width = 300)
+                details.grid(row = x + 1, column = y, padx = 35, pady = 15)
                 
-        add_rec = customtkinter.CTkButton(self, text = "Add record")
-        add_rec.grid(row = 11, column = 2, columnspan = 2, padx = 10, pady = 20, sticky = "nsew")
+        add_rec = customtkinter.CTkButton(self, text = "Add Students", height = 50)
+        add_rec.grid(row = 11, column = 1, columnspan = 2, padx = 35, pady = 20, sticky = "nsew")
