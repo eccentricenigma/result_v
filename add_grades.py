83fb480474ca9190138d5deac6a8c49ccc4daff6
enigma <eccentricenigma0@gmail.com>
2025-02-18 23:36:24 +0100

    minor UI changes

--- add_grades.py
+++ add_grades.py
@@ -13,12 +13,6 @@ class AddGrades(ctk.CTkToplevel):
         #connect to database
         self.conn = sqlite3.connect(os.getenv('DATABASE'))
         self.cursor = self.conn.cursor()
-        
-        #display instructions
-        self.label = ctk.CTkLabel(self, 
-                                            text = "Please ensure to fill in the student's details accordingly",
-                                            font = ("Times", 20, "bold"))
-        self.label.grid(row = 0, column = 0, padx = 10, pady = 10, sticky = "nsew")
             
         #extract courses from database
         self.cursor.execute("SELECT course, subject FROM courses")
@@ -32,12 +26,24 @@ class AddGrades(ctk.CTkToplevel):
         self.headers = ["Name", "Examination number"]
         for x, course in self.courses:
             self.headers.append(course)
+            
+        #display instructions
+        total_columns = len(self.headers)
+        
+        for col in range(total_columns):
+            self.grid_columnconfigure(col, weight=1)
+
+        self.label = ctk.CTkLabel(self, 
+                                            text = "Please ensure to fill in the student's details accordingly",
+                                            font = ("Times", 18, "bold"))
+        self.label.grid(row = 0, column = 0, columnspan=total_columns, padx = 15, pady = 10, sticky = "nsew")
+        
          
         for col_index, header in enumerate(self.headers):
             self.label = ctk.CTkLabel(self,
                                            text = header,
-                                           font = ("Times", 14, "bold"))
-            self.label.grid(row = 1, column = col_index, padx = 10, pady = 10, sticky = "nsew")
+                                           font = ("Times", 18, "bold"))
+            self.label.grid(row = 1, column = col_index, padx = 15, pady = 10, sticky = "nsew")
         
         #display student details under appropriate headers
         for row_index, student in enumerate(self.students):
@@ -48,16 +54,16 @@ class AddGrades(ctk.CTkToplevel):
             self.name_label = ctk.CTkLabel(self,
                                            text = full_name,
                                            font = ("Arial", 14))
-            self.name_label.grid(row = row_index, column = 0,  padx = 10, pady = 10, sticky = "nsew")
+            self.name_label.grid(row = row_index, column = 0,  padx = 15, pady = 10, sticky = "nsew")
             
             self.exam_label = ctk.CTkLabel(self, 
                                            text = examination_number)
-            self.exam_label.grid(row = row_index, column = 1, padx = 10, pady = 10, sticky = "nsew")
+            self.exam_label.grid(row = row_index, column = 1, padx = 15, pady = 10, sticky = "nsew")
             
             # create entries for inputting marks under each course for each student
             for col_index, course in enumerate(self.courses):
                 self.score = ctk.CTkEntry(self,
                                           placeholder_text = "Input score")
-                self.score.grid(row = row_index, column = col_index + 2, padx = 10, pady = 10, sticky = "nsew")
+                self.score.grid(row = row_index, column = col_index + 2, padx = 15, pady = 10, sticky = "nsew")
             
             
